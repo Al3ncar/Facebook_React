cbb662b9047ff60107ac00b149323aa5c1902b92
Al3ncar <alencar29htcss@gmail.com>
2022-02-05 15:16:09 -0300

    Criando sites usando react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import facebook from './Facebook.png';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class menssage extends Component {
+  render(){
+    return(
+      <div>
+        <div className='faceBox'>
+          <img className="faceLogo" src={facebook}/>
+          <h1>Criando um Projeto React</h1>
+          <div className='faceLorem'>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
+export default menssage;
